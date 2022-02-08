4276f1966774d6524b2a437fe2f65bc3eab962e2
christopherlim98 <41635847+christopherlim98@users.noreply.github.com>
2022-02-08 09:50:33 +0800

    chore(limiter): change rate limit to 30 requests per IP per windowMs

--- server/src/middleware/limiter.ts
+++ server/src/middleware/limiter.ts
@@ -3,8 +3,8 @@ import rateLimit from 'express-rate-limit'
 // Set limiter on authenticated routes to reduce burden on servers
 export const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 10, // limit each IP to 60 requests per windowMs
-  handler: function (req, res) {
+  max: 30, // limit each IP to 30 requests per windowMs
+  handler: (req, res) => {
     return res.status(429).json({
       error: 'You sent too many requests. Please wait a while then try again',
     })
